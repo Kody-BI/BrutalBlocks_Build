cfd7f474c640238cbe7b2a06bfc342549e2b8311
Michael Stephens <michaelstephens@Michaels-MBP-16.localdomain>
2024-08-12 21:24:46 -0400

    8-12-24_1

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "DefaultCompany-BrutalBlocks_Unity-1.0";
 const contentToCache = [
-    "Build/7-16-24--1.loader.js",
-    "Build/7-16-24--1.framework.js",
-    "Build/7-16-24--1.data",
-    "Build/7-16-24--1.wasm",
+    "Build/8-12-24--1.loader.js",
+    "Build/8-12-24--1.framework.js",
+    "Build/8-12-24--1.data",
+    "Build/8-12-24--1.wasm",
     "TemplateData/style.css"
 
 ];

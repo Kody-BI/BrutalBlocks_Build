6622fd8ee5ad54b70fa905f596af84da3fa5ec4f
Michael Stephens <michaelstephens@Michaels-MBP-16.localdomain>
2024-08-29 19:43:42 -0400

    memory fix size attempt

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "DefaultCompany-BrutalBlocks_Unity-1.0";
 const contentToCache = [
-    "Build/8-28_24--7.loader.js",
-    "Build/8-28_24--7.framework.js",
-    "Build/8-28_24--7.data",
-    "Build/8-28_24--7.wasm",
+    "Build/8-29_24--1.loader.js",
+    "Build/8-29_24--1.framework.js",
+    "Build/8-29_24--1.data",
+    "Build/8-29_24--1.wasm",
     "TemplateData/style.css"
 
 ];
